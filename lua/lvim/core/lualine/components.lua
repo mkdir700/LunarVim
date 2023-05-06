9252a13c7e59a644a01ed887a0db4523fda64d3b
mkdir700 <mkdir700@gmail.com>
2023-05-06 11:12:25 +0800

    更新文件滚动条展示

--- lua/lvim/core/lualine/components.lua
+++ lua/lvim/core/lualine/components.lua
@@ -168,7 +168,8 @@ return {
     function()
       local current_line = vim.fn.line "."
       local total_lines = vim.fn.line "$"
-      local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+      -- local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+      local chars = { "░░░░", "▒░░░", "█░░░", "█▒░░", "█▒▒░", "█▒▒▒", "██▒▒", "███▒", "████" }
       local line_ratio = current_line / total_lines
       local index = math.ceil(line_ratio * #chars)
       return chars[index]
